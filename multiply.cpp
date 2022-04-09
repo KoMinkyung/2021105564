0ad5da81ba4dab2dac5abfed52315d652a03a994
차가민 <gmcha0323@khu.ac.kr>
2022-04-09 13:51:55 +0900

    edited file

--- multiply.cpp
+++ multiply.cpp
@@ -2,10 +2,11 @@
 int main() {
 	int i = 1;
 	int j = 1;
-	for (i = 1; i < 9; i++) {
+	for (i = 1; i < 10; i++) {
 		for (j = 1; j < 10; j++) {
-			std::cout << i * j << std::endl;
+			std::cout << i << "*" << j << "=" << i * j << std::endl;
 		}
+		std::cout << std::endl;
 	}
 
 }
